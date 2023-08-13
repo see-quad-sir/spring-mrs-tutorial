21d093c5e07d912529630efa7dfc6e1162ecdc13
SeeQuadSir <hp.personalcomputer.mine@gmail.com>
2023-08-13 16:30:53 +0900

    added entities

---
+++ src/main/java/mrs/domain/model/ReservableRoom.java
@@ -0,0 +1,40 @@
+package mrs.domain.model;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+
+import java.io.Serializable;
+
+public class ReservableRoom implements Serializable {
+    @EmbeddedId
+    private ReservableRoomId reservableRoomId;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", insertable = false, updatable = false)
+    @MapsId("roomId")
+    private MeetingRoom meetingRoom;
+
+    public ReservableRoom(ReservableRoomId reservableRoomId) {
+        this.reservableRoomId = reservableRoomId;
+    }
+
+    public ReservableRoom() {}
+
+    public ReservableRoomId getReservableRoomId() {
+        return reservableRoomId;
+    }
+
+    public void setReservableRoomId(ReservableRoomId reservableRoomId) {
+        this.reservableRoomId = reservableRoomId;
+    }
+
+    public MeetingRoom getMeetingRoom() {
+        return meetingRoom;
+    }
+
+    public void setMeetingRoom(MeetingRoom meetingRoom) {
+        this.meetingRoom = meetingRoom;
+    }
+}
